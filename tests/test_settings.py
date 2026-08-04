3c9c63d69112f3430187f262e0a668ae7ad27c61
Eduardo Koketu <eduardo.koketu@gmail.com>
2026-08-04 13:58:42 -0300

    release: bump BetterInventory to 1.3.5

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -911,7 +911,7 @@ def main() -> None:
     localization = lua.execute(LOCALIZATION_PATH.read_text(encoding="utf-8"))
     defaults = {}
 
-    assert data.version == "1.3.4"
+    assert data.version == "1.3.5"
     assert (
         localization["quick_look_card_integration_group"]["en"]
         == "Mod Integration: Quick Look Card"
