6a0145ef5f73bd5512873f1490a2193fcd39164e
Eduardo Koketu <eduardo.koketu@gmail.com>
2026-08-05 18:07:38 -0300

    chore: bump version to 1.7.0

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1345,7 +1345,7 @@ def main() -> None:
     localization = lua.execute(LOCALIZATION_PATH.read_text(encoding="utf-8"))
     defaults = {}
 
-    assert data.version == "1.6.5"
+    assert data.version == "1.7.0"
     assert (
         localization["quick_look_card_integration_group"]["en"]
         == "Mod Integration: Quick Look Card"
